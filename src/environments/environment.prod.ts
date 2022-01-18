cc2bddb9b1f1c2e629b2291bdc1247b51930f6a8
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-01-18 13:24:42 -0300

    Adding the Cognit settings.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  cognito: {
+    userPoolId: 'us-east-2_pptCj2gqV',
+    userPoolWebClientId: '1452opnjll0ldmocs201b1oimu',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  cognito: {
+    userPoolId: 'us-east-2_pptCj2gqV',
+    userPoolWebClientId: '1452opnjll0ldmocs201b1oimu',
+  },
 };
 
 /*
