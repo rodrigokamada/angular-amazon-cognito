85cc77843124e066b9d96101a803d6527bb4046f
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-01-18 13:23:15 -0300

    Adding the ProfileComponent component.

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import { IUser, CognitoService } from '../cognito.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
+})
+export class ProfileComponent implements OnInit {
+
+  loading: boolean;
+  user: IUser;
+
+  constructor(private cognitoService: CognitoService) {
+    this.loading = false;
+    this.user = {} as IUser;
+  }
+
+  public ngOnInit(): void {
+    this.cognitoService.getUser()
+    .then((user: any) => {
+      this.user = user.attributes;
+    });
+  }
+
+  public update(): void {
+    this.loading = true;
+
+    this.cognitoService.updateUser(this.user)
+    .then(() => {
+      this.loading = false;
+    }).catch(() => {
+      this.loading = false;
+    });
+  }
+
+}
