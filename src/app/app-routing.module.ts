f283bc7befa2e050c1499d2c45ffc6cfd7e241f9
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-01-18 13:43:28 -0300

    Adding the routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProfileComponent } from './profile/profile.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'signIn',
+    pathMatch: 'full',
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'signIn',
+    component: SignInComponent,
+  },
+  {
+    path: 'signUp',
+    component: SignUpComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'signIn',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
