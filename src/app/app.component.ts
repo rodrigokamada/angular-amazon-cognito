d448549afcc47c30350dfcdfdfabf69db90acffc
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-01-18 13:45:17 -0300

    Adding the isAuthenticated and signOut methods.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { CognitoService } from './cognito.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-cognito';
+
+  constructor(private router: Router,
+              private cognitoService: CognitoService) {
+  }
+
+  public isAuthenticated(): boolean {
+    return this.cognitoService.isAuthenticated();
+  }
+
+  public signOut(): void {
+    this.cognitoService.signOut()
+    .then(() => {
+      this.router.navigate(['/signIn']);
+    });
+  }
+
 }
