fda409c7946a036ccb8a93ffb5cbbd530662b682
Rodrigo Kamada <rodrigo@kamada.com.br>
2022-01-20 22:46:40 -0300

    Changing the repository name.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-cognito'),
+      dir: require('path').join(__dirname, './coverage/angular-amazon-cognito'),
       subdir: '.',
       reporters: [
         { type: 'html' },
